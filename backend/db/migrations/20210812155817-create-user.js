c706a27d6262b1b938c6aa3a96269045f8ed7b47
Cam Chandler <camchandler15@gmail.com>
2021-08-25 12:04:35 -0400

    added profile ppicture url to migration

--- backend/db/migrations/20210812155817-create-user.js
+++ backend/db/migrations/20210812155817-create-user.js
@@ -22,6 +22,10 @@ module.exports = {
         type: Sequelize.STRING.BINARY,
         allowNull: false,
       },
+      profilePictureUrl:{
+        type: Sequelize.STRING,
+        allowNull: false
+      },
       createdAt: {
         allowNull: false,
         type: Sequelize.DATE,
