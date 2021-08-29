a8c268c4146e7a3147730afd8c3a5e379aa95954
Cam Chandler <camchandler15@gmail.com>
2021-08-28 20:18:10 -0400

    heroku test

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider as ReduxProvider } from 'react-redux';
-import './index.css';
+// import './index.css';
 import App from './App';
 import configureStore from './store';
 import { restoreCSRF, csrfFetch } from './store/csrf';
